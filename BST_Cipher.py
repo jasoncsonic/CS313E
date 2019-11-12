8e0232b701e4190b0f93d0c76b006ee1a71b6870
Peyton Breech <peyton.breech16@gmail.com>
2019-11-12 16:36:24 -0600

    Fixed

--- BST_Cipher.py
+++ BST_Cipher.py
@@ -14,37 +14,48 @@
 
 #  Date Last Modified: 11/12/2019 
 
+class Node (object):
+    def __init__ (self, data):
+        self.data = data
+        self.lchild = None
+        self.rchild = None
+
+    def __str__ (self):
+        return (f"{self.data}")
+
 class Tree (object):
-  # the init() function creates the binary search tree with the
-  # encryption string. If the encryption string contains any
-  # character other than the characters 'a' through 'z' or the
-  # space character drop that character.
-  def __init__ (self, encrypt_str):
+    # the init() function creates the binary search tree with the
+    # encryption string. If the encryption string contains any
+    # character other than the characters 'a' through 'z' or the
+    # space character drop that character.
+    def __init__ (self, encrypt_str):
+        self.root = None
+        [self.insert(word) for word in encrypt_str]
 
-  # the insert() function adds a node containing a character in
-  # the binary search tree. If the character already exists, it
-  # does not add that character. There are no duplicate characters
-  # in the binary search tree.
-  def insert (self, ch):
+    # the insert() function adds a node containing a character in
+    # the binary search tree. If the character already exists, it
+    # does not add that character. There are no duplicate characters
+    # in the binary search tree.
+    def insert (self, ch):
 
-  # the search() function will search for a character in the binary
-  # search tree and return a string containing a series of lefts
-  # (<) and rights (>) needed to reach that character. It will
-  # return a blank string if the character does not exist in the tree.
-  # It will return * if the character is the root of the tree.
-  def search (self, ch):
+    # the search() function will search for a character in the binary
+    # search tree and return a string containing a series of lefts
+    # (<) and rights (>) needed to reach that character. It will
+    # return a blank string if the character does not exist in the tree.
+    # It will return * if the character is the root of the tree.
+    def search (self, ch):
 
-  # the traverse() function will take string composed of a series of
-  # lefts (<) and rights (>) and return the corresponding 
-  # character in the binary search tree. It will return an empty string
-  # if the input parameter does not lead to a valid character in the tree.
-  def traverse (self, st):
+    # the traverse() function will take string composed of a series of
+    # lefts (<) and rights (>) and return the corresponding 
+    # character in the binary search tree. It will return an empty string
+    # if the input parameter does not lead to a valid character in the tree.
+    def traverse (self, st):
 
-  # the encrypt() function will take a string as input parameter, convert
-  # it to lower case, and return the encrypted string. It will ignore
-  # all digits, punctuation marks, and special characters.
-  def encrypt (self, st):
+    # the encrypt() function will take a string as input parameter, convert
+    # it to lower case, and return the encrypted string. It will ignore
+    # all digits, punctuation marks, and special characters.
+    def encrypt (self, st):
 
-  # the decrypt() function will take a string as input parameter, and
-  # return the decrypted string.
-  def decrypt (self, st):
+    # the decrypt() function will take a string as input parameter, and
+    # return the decrypted string.
+    def decrypt (self, st):
