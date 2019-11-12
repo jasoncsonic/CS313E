9500ba5cc83e106418c2ddc96bd8e495ee25ee85
Peyton Breech <peyton.breech16@gmail.com>
2019-11-12 16:12:11 -0600

    Assignment 20 Shell

---
+++ BST_Cipher.py
@@ -0,0 +1,50 @@
+#  File: BST_Cipher.py 
+
+#  Description: 
+
+#  Student's Name: Peyton Breech
+
+#  Student's UT EID: pb23489
+
+#  Course Name: CS 313E 
+
+#  Unique Number: 50205
+
+#  Date Created: 11/12/2019
+
+#  Date Last Modified: 11/12/2019 
+
+class Tree (object):
+  # the init() function creates the binary search tree with the
+  # encryption string. If the encryption string contains any
+  # character other than the characters 'a' through 'z' or the
+  # space character drop that character.
+  def __init__ (self, encrypt_str):
+
+  # the insert() function adds a node containing a character in
+  # the binary search tree. If the character already exists, it
+  # does not add that character. There are no duplicate characters
+  # in the binary search tree.
+  def insert (self, ch):
+
+  # the search() function will search for a character in the binary
+  # search tree and return a string containing a series of lefts
+  # (<) and rights (>) needed to reach that character. It will
+  # return a blank string if the character does not exist in the tree.
+  # It will return * if the character is the root of the tree.
+  def search (self, ch):
+
+  # the traverse() function will take string composed of a series of
+  # lefts (<) and rights (>) and return the corresponding 
+  # character in the binary search tree. It will return an empty string
+  # if the input parameter does not lead to a valid character in the tree.
+  def traverse (self, st):
+
+  # the encrypt() function will take a string as input parameter, convert
+  # it to lower case, and return the encrypted string. It will ignore
+  # all digits, punctuation marks, and special characters.
+  def encrypt (self, st):
+
+  # the decrypt() function will take a string as input parameter, and
+  # return the decrypted string.
+  def decrypt (self, st):
